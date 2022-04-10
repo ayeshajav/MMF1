a668e4c410359ba7a24c0bd9cee46415d8e3ee95
Ayesha Javed <101297266+ayeshajav@users.noreply.github.com>
2022-04-10 22:32:58 +0000

    Added 03_number_checker.py file

--- base.py
+++ base.py
@@ -28,9 +28,38 @@ def not_blank(question):
 
 #Loop to get ticket details 
 
-  #Get name (can't be blank)
-  name = not_blank("Name: ")
+#start of loop 
 
+#initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5 
+
+while name != "xxx" and count < MAX_TICKETS:  
+
+    #tells user how many seats are left
+    if count < 4: 
+        print("You have {} seats "
+              "left".format(MAX_TICKETS - count))
+
+    #warns user that only one seat is left!
+    else: 
+        print("*** There is ONE seat left!! ***")
+    #Get details...
+
+    #Get name (can't be blank)
+    name = not_blank("Name: ")
+    count += 1 
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets.  \n"
+          "There are {} places still available"
+          .format(count, MAX_TICKETS - count))
+          
+  
   #Get age (betweem 12 and 130)
 
   #calculate ticket price 
